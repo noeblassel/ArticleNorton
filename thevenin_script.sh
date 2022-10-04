88f11e71babcc946fc7fb52653250b4095cb0673
Noe Blassel <nblassel@PL1179-PRO.paris.inria.fr>
2022-10-04 18:24:45 +0200

    change splitting to BAOAB

--- thevenin_script.sh
+++ thevenin_script.sh
@@ -2,5 +2,5 @@
 
 for N in `seq 6 15`
 do
-nohup /libre/blasseln/julia-1.8.2/bin/julia --threads=16 thevenin_shear.jl 0.8 0.7 1e-3 1.0 0.3 SINUSOIDAL 100.0 1000 $N OBABO 2.5 > nohup.out &
+nohup /libre/blasseln/julia-1.8.2/bin/julia --threads=16 thevenin_shear.jl 0.8 0.7 1e-3 1.0 0.3 SINUSOIDAL 100.0 1000 $N BAOAB 2.5 > nohup.out &
 done
