67262092c88ccc1ea07ade40f11e654edac78d59
Noe Blassel <nblassel@PL1179-PRO.paris.inria.fr>
2022-10-05 09:17:29 +0200

    fix s series

--- thermo_results/crunch_results.jl
+++ thermo_results/crunch_results.jl
@@ -30,7 +30,7 @@ for Npd in 6:15
     println(Npd)
     S_file= (method == "THEVENIN") ? open("thevenin_response_SINUSOIDAL_0.3_$Npd.out","r") : open("../norton_forcing_SINUSOIDAL_0.1_$Npd.out","r")
     S_series=reinterpret(Float64,read(S_series))
-    close(S_series)
+    close(S_file)
     T_file=(method == "THEVENIN") ? open("thevenin_temp_SINUSOIDAL_0.3_$Npd.out","r") : open("../norton_temp_SINUSOIDAL_0.1_$Npd.out","r")
     T_series=reinterpret(Float64,read(T_file))
     close(T_file)
