7330a6858e779e4b6b72987c80e4baf1e5ff8ef2
Noe Blassel <noe.blassel@enpc.fr>
2022-10-11 11:08:19 +0200

    process histograms

---
+++ thermo_results/proc_histograms.jl
@@ -0,0 +1,26 @@
+#!/libre/blasseln/julia-1.8.2/bin/julia
+
+n_bins, files...=ARGS
+
+if !isdir("histograms")
+    mkdir("histograms")
+end
+
+for f in files
+    println(f)
+    fh=open(f,"r")
+    series=reinterpret(Float64,read(fh))
+    close(fh)
+    m,M=minimum(series),maximum(series)
+    dx=(M-m)/n_bins
+    hist=zeros(n_bins)
+
+    ts = (M .- x) / (M-m)
+    is =floor.(Int64,t*n_bins)
+    map(i-> hist[i]+=1,is)
+
+    output_f=open("histograms/$f","w")
+    println(output_f,"$m $M $n_bins")
+    print(output_f,join(hist,", "))
+    close(output_f)
+end
