01eae9ef26a9d652d941997b5024cfea833df2a7
Noe Blassel <noe.blassel@enpc.fr>
2022-10-24 13:57:47 +0200

    thermo limit larger r

--- norton_script.sh
+++ norton_script.sh
@@ -3,5 +3,5 @@ Nmin=$1
 Nmax=$2
 for N in `seq $Nmin 2 $Nmax`
 do
-nohup /libre/blasseln/julia-1.8.2/bin/julia --threads=16 norton_shear.jl 0.8 0.7 1e-3 1.0 0.1 SINUSOIDAL 100.0 1000 $N BAOAB 2.5 2.0 0.5 > nohup.out &
+nohup /libre/blasseln/julia-1.8.2/bin/julia --threads=16 norton_shear.jl 0.8 0.7 1e-3 1.0 0.2 SINUSOIDAL 100.0 1000 $N BAOAB 2.5 2.0 0.5 > nohup.out &
 done
