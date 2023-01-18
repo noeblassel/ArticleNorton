cf846e363d7090680cb60330b8b1051b898626e8
noeblassel <noe.blassel@gmail.com>
2023-01-18 14:10:33 +0100

    zero forcing

--- thevenin_script.sh
+++ thevenin_script.sh
@@ -4,5 +4,5 @@ Nmin=$1
 Nmax=$2
 for N in `seq $Nmin 2 $Nmax`
 do
-nohup /libre/blasseln/julia-1.8.2/bin/julia --threads=16 thevenin_shear.jl 0.8 0.7 1e-3 1.0 0.5 SINUSOIDAL 100.0 100 $N BAOAB 2.5 2.0 0.5 > nohup.out &
+nohup /libre/blasseln/julia-1.8.2/bin/julia --threads=16 thevenin_shear.jl 0.8 0.7 1e-3 1.0 0.0 SINUSOIDAL 100.0 100 $N BAOAB 2.5 2.0 0.5 > nohup.out &
 done
