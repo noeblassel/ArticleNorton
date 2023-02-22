498f7d3196b4bc8128339b64004948c6709d2d0d
noeblassel <noe.blassel@gmail.com>
2023-02-22 10:18:34 +0100

    reduce timestep

--- thevenin_response_script.sh
+++ thevenin_response_script.sh
@@ -6,10 +6,9 @@ eta_inc=$3
 
 N=$4
 
-dt = $5
 
 for eta in `LANG=en_US seq $eta_min $eta_inc $eta_max`
 do
-nohup /libre/blasseln/julia-1.8.2/bin/julia --threads=16 thevenin_shear.jl 0.8 0.7 $dt 1.0 $eta SINUSOIDAL 100.0 100 $N BAOAB 2.5 2.0 0.5 > nohup.out &
+nohup /libre/blasseln/julia-1.8.2/bin/julia --threads=16 thevenin_shear.jl 0.8 0.7 5e-4 1.0 $eta SINUSOIDAL 100.0 100 $N BAOAB 2.5 2.0 0.5 > nohup.out &
 echo $eta
 done
