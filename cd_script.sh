f7cc663f524d7e57bcf03f17b3cfb720aca44b47
noeblassel <noe.blassel@gmail.com>
2023-03-17 18:36:25 +0100

    longer sims

--- cd_script.sh
+++ cd_script.sh
@@ -4,14 +4,14 @@ Nmin=$1
 Nmax=$2
 method=$3
 
-for N in `seq $Nmin 2 $Nmax`
+for N in `seq $Nmin 10 $Nmax`
 do
 
 if [ $method == "norton" ]
 
 then
-    nohup /libre/blasseln/julia-1.8.2/bin/julia --threads=16 norton_cd.jl 1.25 0.6 5e-3 1.0 1.21 10.0 1000 $N BAOAB > nohup.out &
+    nohup /libre/blasseln/julia-1.8.2/bin/julia --threads=16 norton_cd.jl 1.25 0.6 5e-3 1.0 1.21 10.0 100000 $N BAOAB > nohup.out &
 else
-    nohup /libre/blasseln/julia-1.8.2/bin/julia --threads=16 thevenin_cd.jl 1.25 0.6 5e-3 1.0 10.0  10.0 1000 $N BAOAB > nohup.out &
+    nohup /libre/blasseln/julia-1.8.2/bin/julia --threads=16 thevenin_cd.jl 1.25 0.6 5e-3 1.0 10.0  10.0 100000 $N BAOAB > nohup.out &
 fi
 done
