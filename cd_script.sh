3a2a0dd4cf07e355083d6cb81c312cd60543cedc
noeblassel <noe.blassel@gmail.com>
2023-03-17 18:36:49 +0100

    lower thread count

--- cd_script.sh
+++ cd_script.sh
@@ -10,8 +10,8 @@ do
 if [ $method == "norton" ]
 
 then
-    nohup /libre/blasseln/julia-1.8.2/bin/julia --threads=16 norton_cd.jl 1.25 0.6 5e-3 1.0 1.21 10.0 100000 $N BAOAB > nohup.out &
+    nohup /libre/blasseln/julia-1.8.2/bin/julia --threads=4 norton_cd.jl 1.25 0.6 5e-3 1.0 1.21 10.0 100000 $N BAOAB > nohup.out &
 else
-    nohup /libre/blasseln/julia-1.8.2/bin/julia --threads=16 thevenin_cd.jl 1.25 0.6 5e-3 1.0 10.0  10.0 100000 $N BAOAB > nohup.out &
+    nohup /libre/blasseln/julia-1.8.2/bin/julia --threads=4 thevenin_cd.jl 1.25 0.6 5e-3 1.0 10.0  10.0 100000 $N BAOAB > nohup.out &
 fi
 done
