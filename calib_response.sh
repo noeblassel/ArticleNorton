6f86a4e19344ad5fbc9020b4fe969d6f1052f219
noeblassel <noe.blassel@gmail.com>
2023-03-20 15:19:42 +0100

    fix response script

--- calib_response.sh
+++ calib_response.sh
@@ -1,4 +1,4 @@
 for eta in 0.1 0.2 0.5 1.0 2.0 5.0 10.0 20.0
 do
-nohup /libre/blasseln/julia-1.8.2/bin/julia --threads=8 thevenin_cd.jl 2.5 1.0 1e-3 1.0 10.0 10.0 1000 400 BAOAB > nohup.out &
+nohup /libre/blasseln/julia-1.8.2/bin/julia --threads=8 thevenin_cd.jl 2.5 1.0 1e-3 1.0 $eta 10.0 1000 400 BAOAB > nohup.out &
 done
