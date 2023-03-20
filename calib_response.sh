f677098a71480b1e23cdda7b85a08fb83f661751
noeblassel <noe.blassel@gmail.com>
2023-03-20 16:15:27 +0100

    calibrate nl response

--- calib_response.sh
+++ calib_response.sh
@@ -1,4 +1,4 @@
-for eta in 100.0 200.0 300.0 400.0 500.0 600.0 700.0 
+for eta in 1000.0 1500.0 2000.0 2500.0 3000.0 3500.0 4000.0 
 do
 nohup /libre/blasseln/julia-1.8.2/bin/julia --threads=8 thevenin_cd.jl 2.5 1.0 1e-3 1.0 $eta 10.0 1000 400 BAOAB > nohup.out &
 done
